63935c903bc786b73b8ee729a47fd6d2bc976abd
YangJH <jhaugusty@gmail.com>
2019-06-15 17:22:11 +0900

    lint config
    - add eslint
    - add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+const jsExtensions = ['.js', '.jsx'];
+const tsExtensions = ['.ts', '.tsx'];
+const allExtensions = jsExtensions.concat(tsExtensions);
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb', 'prettier/@typescript-eslint', 'prettier'],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/extensions': allExtensions,
+    'import/parsers': {
+      '@typescript-eslint/parser': tsExtensions,
+    },
+    'import/resolver': {
+      node: {
+        extensions: allExtensions,
+      },
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.ts', 'tsx'] }],
+    'react/jsx-one-expression-per-line': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    'spaced-comment': ['error', 'always', { markers: ['/'] }],
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+};
